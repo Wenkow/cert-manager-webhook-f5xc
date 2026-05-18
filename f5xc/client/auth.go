66b5c3230bbd6c7fc56b5764e633c94450f3728f
Martin Kylian <martin.kylian@gmail.com>
2026-05-18 22:15:12 +0200

    feat: add CertAuth with P12 certificate parsing and mTLS transport

--- f5xc/client/auth.go
+++ f5xc/client/auth.go
@@ -1,12 +1,20 @@
 package client
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"net/http"
+
+	pkcs12 "software.sslmate.com/src/go-pkcs12"
 )
 
 type Authenticator interface {
 	Apply(req *http.Request) error
+}
+
+type TransportProvider interface {
+	Transport() *http.Transport
 }
 
 type TokenAuth struct {
@@ -19,4 +27,49 @@ func (a *TokenAuth) Apply(req *http.Request) error {
 	}
 	req.Header.Set("Authorization", "APIToken "+a.Token)
 	return nil
+}
+
+type CertAuth struct {
+	transport *http.Transport
+}
+
+func NewCertAuth(p12Data []byte, password string) (*CertAuth, error) {
+	if len(p12Data) == 0 {
+		return nil, errors.New("f5xc: P12 certificate data is empty")
+	}
+
+	privateKey, leafCert, caCerts, err := pkcs12.DecodeChain(p12Data, password)
+	if err != nil {
+		return nil, errors.New("f5xc: failed to decode P12 certificate: " + err.Error())
+	}
+
+	tlsCert := tls.Certificate{
+		Certificate: [][]byte{leafCert.Raw},
+		PrivateKey:  privateKey,
+	}
+	for _, ca := range caCerts {
+		tlsCert.Certificate = append(tlsCert.Certificate, ca.Raw)
+	}
+
+	caPool := x509.NewCertPool()
+	for _, ca := range caCerts {
+		caPool.AddCert(ca)
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		RootCAs:      caPool,
+	}
+
+	return &CertAuth{
+		transport: &http.Transport{TLSClientConfig: tlsConfig},
+	}, nil
+}
+
+func (a *CertAuth) Apply(req *http.Request) error {
+	return nil
+}
+
+func (a *CertAuth) Transport() *http.Transport {
+	return a.transport
 }
